cb13104a2e9e994e6653f359e6ee2079c01e8995
Mubin <fathummubin26@gmail.com>
2026-02-12 00:13:29 +0600

    Update backend and frontend files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import PokemonDetail from './pages/PokemonDetail';
+import SplashIntro from './components/SplashIntro';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pokemon/:name" element={<PokemonDetail />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        {showSplash ? (
+          <SplashIntro key="splash" onComplete={() => setShowSplash(false)} />
+        ) : (
+          <div key="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/pokemon/:name" element={<PokemonDetail />} />
+            </Routes>
+          </div>
+        )}
+      </AnimatePresence>
     </BrowserRouter>
   );
 }
